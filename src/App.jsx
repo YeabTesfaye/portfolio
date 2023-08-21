5759a98b912f8a4cf03838ef8d8b391018479785
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-08-21 11:41:46 +0300

    fixied testimonials import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Navbar,Contact,Testimonials ,Experieance,About,Footer,Header, Services } from "./components"
+import { Navbar,Contact ,Experieance,About,Footer,Header, Services } from "./components"
 import Portfolio from "./components/portfolio/Portfolio.jsx"
+import Testimonials from './components/testimonials/Testimonials.jsx'
 function App() {
 
   return(<>
